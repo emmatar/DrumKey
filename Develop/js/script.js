cfd7f76c9fa543debfe08c7f37cf0d902277a448
Elijah Matar <m.matar515@gmail.com>
2024-01-19 21:48:51 -0600

    HTML/CSS/JavaScript for Drumkit Project

---
+++ Develop/js/script.js
@@ -0,0 +1,22 @@
+function sounds(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add('pressed')
+}
+
+function transitionEnd(e) {
+    if(e.propertyName !== "transform") {
+        return;
+    }
+    this.classList.remove('pressed')
+}
+
+const key = document.querySelectorAll('.key')
+key.forEach(transition => {
+    transition.addEventListener('transitionend', transitionEnd)
+});
+
+
+window.addEventListener('keydown', sounds)
